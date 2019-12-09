6c895499ed41550d30ad9ba6400c977de3819009
ga10is <ga10is1831@gmail.com>
2019-12-09 23:32:04 +0900

    - add config

---
+++ src/config.py
@@ -0,0 +1,43 @@
+import torch
+import os
+
+# device
+DEVICE = torch.device("cuda:0")
+
+# paths
+INPUT = '/kaggle/input/pku-autonomous-driving'
+TRAIN_CSV = os.path.join(INPUT, 'train.csv')
+TRAIN_IMAGE = os.path.join(INPUT, 'train_images')
+TRAIN_MASK = os.path.join(INPUT, 'train_masks')
+TEST_IMAGE = os.path.join(INPUT, 'test_images')
+TEST_MASK = os.path.join(INPUT, 'test_masks')
+
+# train/valid/predict
+EPOCHS = 2
+BATCH_SIZE = 4
+NUM_WORKERS = 8
+PRINT_FREQ = 10
+
+# image
+# aspect ratio(approx): 4: 5
+IMG_SIZE = (2710, 3384)
+INPUT_SIZE = (256 * 4, 256 * 5)
+
+# object detection
+MAX_OBJ = 50
+MODEL_SCALE = 4
+OUTPUT_WIDTH = INPUT_SIZE[1] // MODEL_SCALE
+OUTPUT_HEIGHT = INPUT_SIZE[0] // MODEL_SCALE
+
+# loss weights
+HM_WEIGHT = 1
+OFFSET_WEIGHT = 1
+QUART_WEIGHT = 1
+DEPTH_WEIGHT = 1
+ROTATE_WEIGHT = 1
+
+# optimizer
+ADAM_LR = 1e-5
+
+# clear duplicates
+DISTANCE_THRESH_CLEAR = 2
