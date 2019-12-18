a3f3e28fe93cd1e75d490d7ec5af3a702f887624
ga10is <ga10is1831@gmail.com>
2019-12-18 22:23:55 +0900

    add debug print

--- src/train.py
+++ src/train.py
@@ -1,13 +1,15 @@
+import pprint
 from tqdm import tqdm
 import numpy as np
 import pandas as pd
 import torch
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
+from apex import amp
 
 from . import config
 from .data.dataset import CarDataset, car_collate_fn, get_train_transform
-from .model.model import decode_eval, ResNet
+from .model.model import decode_eval, ResNet, _nms
 from .model.metrics import car_map, AverageMeter
 from .model.loss import CarLoss, _sigmoid
 
@@ -24,7 +26,7 @@ def train():
         df_trn, config.TRAIN_IMAGE, 'train', get_train_transform())
     train_loader = DataLoader(train_dataset,
                               batch_size=config.BATCH_SIZE,
-                              shuffle=False,
+                              shuffle=True,
                               num_workers=config.NUM_WORKERS,
                               pin_memory=True,
                               drop_last=True,
@@ -37,7 +39,7 @@ def train():
                               shuffle=False,
                               num_workers=config.NUM_WORKERS,
                               pin_memory=True,
-                              drop_last=True,
+                              drop_last=False,
                               collate_fn=car_collate_fn
                               )
 
@@ -56,6 +58,7 @@ def init_model():
     optimizer = torch.optim.Adam(
         [{'params': model.parameters()}], lr=config.ADAM_LR)
     scheduler = None
+    # model, optimizer = amp.initialize(model, optimizer, opt_level="O1")
 
     return model, criterion, optimizer, scheduler
 
@@ -72,17 +75,21 @@ def train_one_epoch(epoch, model, loader, criterion, optimizer):
             logits = model(data['image'])
             loss, loss_stats = criterion(logits, data)
 
-            optimizer.zero_grad()
             loss.backward()
-            optimizer.step()
+            # with amp.scale_loss(loss, optimizer) as scaled_loss:
+            # scaled_loss.backward()
+            if (i + 1) % config.ACC_ITER == 0:
+                optimizer.step()
+                optimizer.zero_grad()
 
         for k in loss_stats:
             loss_meters[k].update(loss_stats[k].item(), batch_size)
 
         print_loss(i, loss_meters)
         if (i + 1) % 10 == 0:
-            # use last output
+            # last output of Hourglass is used
             show_heatmap(data, logits[-1])
+            print_decode(data, logits[-1])
 
 
 def to_gpu(data):
@@ -102,6 +109,16 @@ def print_loss(idx, loss_meters):
               loss_meters['loss_rotate'].avg)
 
 
+def print_decode(data, output):
+    gt = data['gt'][0]
+    depth = data['depth'][0].detach().cpu().numpy()
+    decode_output = decode_eval(output, k=10)[0]
+    pprint.pprint(gt)
+    print('depth')
+    print(depth)
+    pprint.pprint(decode_output)
+
+
 def show_heatmap(data, output):
     img = data['image'][0].detach().cpu().numpy()
     # transpose (c, h, w) -> (h, w, c)
@@ -111,18 +128,24 @@ def show_heatmap(data, output):
     mean = np.array([0.485, 0.456, 0.406]).reshape(1, 1, 3)
     img = img * std + mean
 
-    heatmap = output['heatmap'][0].detach()
-    heatmap = _sigmoid(heatmap).cpu().numpy()
+    heatmaps = output['heatmap'].detach()
+    heatmap = _sigmoid(heatmaps[0]).cpu().numpy()
+    nms_heatmap = _nms(heatmaps)[0].cpu().numpy()
     # transform (c, h, w) -> (h, w)
     heatmap = heatmap[0, :, :]
+    nms_heatmap = nms_heatmap[0, :, :]
+
+    heatmap = cv2.applyColorMap(np.uint8(255*heatmap), cv2.COLORMAP_JET)
+    heatmap = cv2.cvtColor(cam, cv2.COLOR_BGR2RGB)
 
     gt_heatmap = data['heatmap'][0].detach()
     gt_heatmap = gt_heatmap.cpu().numpy()
 
-    fig, axes = plt.subplots(1, 3, figsize=(15, 15))
-    axes[0].imshow(img)
-    axes[1].imshow(heatmap)
-    axes[2].imshow(gt_heatmap)
+    fig, axes = plt.subplots(2, 2, figsize=(20, 20))
+    axes[0, 0].imshow(img)
+    axes[0, 1].imshow(gt_heatmap)
+    axes[1, 0].imshow(heatmap)
+    axes[1, 1].imshow(nms_heatmap)
     plt.show()
 
 
@@ -142,7 +165,7 @@ def valid_one_epoch(epoch, model, loader, criterion):
             # loss = criterion(logits, data)
             # loss_meter.update(loss.item(), batch_size)
 
-        # use last output
+        # last output of Hourglass is used
         pred_batch_list = decode_eval(logits[-1], k=config.MAX_OBJ)
         img_ids = data['ImageId']
         pred_batch_dict = dict(zip(img_ids, pred_batch_list))
